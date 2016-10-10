b85130942990343800eed6e6a702d6b952438ce5
alxdarksage <alx.dark@sagebase.org>
2016-10-10 10:22:03 -0700

    Ensuring swagger validation will fail script if it fails; fixing typo.

--- verify.sh
+++ verify.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 ./node_modules/.bin/swagger validate _site/swagger.json
+if [ $? -eq 0 ]
+then
+  echo "Successfully validated swagger.json"
+else
+  echo "swagger.json invalid"
+  exit 1
+fi
 rm -rf java/
-java -jar swagger-codegen-cli.jar generate -i _/site/swagger.json -l java --library retrofit2 -o java/
+java -jar swagger-codegen-cli.jar generate -i _site/swagger.json -l java --library retrofit2 -o java/
 cd java
 mvn compile
