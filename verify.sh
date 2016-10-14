f3a22fdbb27ea4a36eebd70ec83fbbf3f5266d5f
alxdarksage <alx.dark@sagebase.org>
2016-10-14 09:34:12 -0700

    Adding inherited properties to sub-types documentation.

--- verify.sh
+++ verify.sh
@@ -13,16 +13,19 @@ rm -rf java/
 java -jar swagger-codegen-cli.jar generate -i swagger.json -l java --library retrofit2 -o java/
 cd java
 mvn compile
+if [ $? -eq 0 ]
+then
+  echo "Successfully validated Java code generation"
+else
+  echo "swagger.json invalid"
+  exit 1
+fi
 mvn javadoc:javadoc
 if [ $? -eq 0 ]
 then
-  echo "Successfully validated swagger.json"
+  echo "Successfully validated Javadoc generation"
 else
   echo "swagger.json invalid"
   exit 1
 fi
 cd ../
-# rm -rf java/
-# rm swagger-codegen-cli.jar
-# rm -rf node_modules/
-bundle exec jekyll build
