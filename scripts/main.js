6e345c9a9aca235ee86cb4002c2ab03890586bc6
alxdarksage <alx.dark@sagebase.org>
2016-10-19 10:24:13 -0700

    Minor updates to documentation (roles).

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
-// Activate the current navigation link
+// Activate the current navigation link, indicate external links
 var items = document.body.querySelectorAll("a");
 for (var i=0; i < items.length; i++) {
     var item = items[i];
     var href = item.getAttribute('data-filepath') || item.getAttribute('href');
-    if (/http[s]?\:/.test(href) && !item.hasAttribute('no-ext')) {
-        var icon = document.createElement("i");
-        icon.className = "ui small external icon";
-        icon.textContent = " ";
-        item.appendChild(document.createTextNode(" "));
-        item.appendChild(icon);
-        item.target = "_blank";
+    if (!item.hasAttribute('no-ext')) {
+        if (/http[s]?\:/.test(href)) {
+            iconify(item, "external");
+            item.target = "_blank";
+        } else if (/mailto\:/.test(href)) {
+            iconify(item, "mail outline");
+        }
     }
     if (document.location.pathname.indexOf(href) > -1) {
         item.classList.add("active");
@@ -24,4 +24,11 @@ for (var i=0; i < items.length; i++) {
     div.className = "table-wrapper";
     item.parentNode.replaceChild(div, item);
     div.appendChild(item);
+}
+function iconify(item, iconName) {
+    var icon = document.createElement("i");
+    icon.className = "ui small "+iconName+" icon";
+    icon.textContent = "";
+    item.appendChild(document.createTextNode(" "));
+    item.appendChild(icon);
 }
